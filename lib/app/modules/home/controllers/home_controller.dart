87f88c41761688bef8015440765e43d4c48dee03
mahidul-islam <mizihan84@gmail.com>
2023-04-30 18:39:20 +0600

    web and ios realtime database connection working

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,40 @@
+import 'dart:convert';
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  DatabaseReference? _database;
+  DatabaseReference? _cache;
 
-  final count = 0.obs;
+  RxMap<String, dynamic> map = {'Zihan': 'vai'}.obs;
+
   @override
   void onInit() {
+    FirebaseDatabase.instance.databaseURL =
+        'https://history-collab-default-rtdb.asia-southeast1.firebasedatabase.app/';
+    _database = FirebaseDatabase.instance.ref();
+    _cache = _database?.child('sirah/');
+    _cache?.onValue.listen(
+      (DatabaseEvent event) => checkAndUpdate(event),
+    );
     super.onInit();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  void checkAndUpdate(DatabaseEvent event) {
+    final Map<String, dynamic>? cacheMap =
+        json.decode(json.encode(event.snapshot.value));
+    map.value = cacheMap ?? {'New': 'default'};
+    if (kDebugMode) {
+      print(cacheMap);
+    }
   }
 
   @override
   void onClose() {
+    _database = null;
+    _cache = null;
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -8,17 +8,19 @@ class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+    return Obx(() {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('HomeView'),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: Center(
+          child: Text(
+            controller.map.toString(),
+            style: const TextStyle(fontSize: 20),
+          ),
+        ),
+      );
+    });
   }
 }
