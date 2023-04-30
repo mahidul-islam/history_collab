fcd4049243db3ebc8f46a65b662e60e8fa7608fc
mahidul-islam <mizihan84@gmail.com>
2023-04-30 17:48:26 +0600

    initial commit with firebase setup

---
+++ android/app/src/main/kotlin/com/arrijal/history_collab/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.arrijal.history_collab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:history_collab/firebase_options.dart';
+
+import 'app/routes/app_pages.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    GetMaterialApp(
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    ),
+  );
+}
